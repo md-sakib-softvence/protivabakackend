b5d184f4eab55f0fdd632da72414e4d26c45edf1
Mahedi Hasan Jihad <mahdijihad001@gmail.com>
2026-04-07 14:15:05 +0600

    Fix the Client And provider info Problem

---
+++ prisma/migrations/20260407081046_add_provider_relation/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "bookings" ADD COLUMN     "userId" TEXT;
+
+-- AddForeignKey
+ALTER TABLE "bookings" ADD CONSTRAINT "bookings_providerId_fkey" FOREIGN KEY ("providerId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bookings" ADD CONSTRAINT "bookings_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
